a5a02d9b14e38101ff42ccaab8dad1c0f722fba4
Iamneo <techadmin@iamneo.ai>
2023-09-30 08:21:04 +0000

    Backup Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CalculatorS from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
